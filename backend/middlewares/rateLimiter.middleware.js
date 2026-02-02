1d9f1b6553994173a2491ca9d68a75dbee1f3f81
Anos714 <rahul_sain0@outlook.com>
2026-02-02 20:51:57 +0530

    make separate rate limiters middlewares for auth routes

--- backend/middlewares/rateLimiter.middleware.js
+++ backend/middlewares/rateLimiter.middleware.js
@@ -1,6 +1,17 @@
 import rateLimit from "express-rate-limit";
 
-export const authLimiter = rateLimit({
+export const registerLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 3,
+  message: {
+    success: false,
+    msg: "Too many login attempts. Try again after 1 hour",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+export const loginLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 5,
   message: {
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/auth.controller.js";
-import { authLimiter } from "../middlewares/rateLimiter.middleware.js";
+import {
+  loginLimiter,
+  registerLimiter,
+} from "../middlewares/rateLimiter.middleware.js";
 
 const router = Router();
 
-router.post("/register", authLimiter, registerUser);
-router.post("/login", authLimiter, loginUser);
+router.post("/register", registerLimiter, registerUser);
+router.post("/login", loginLimiter, loginUser);
 
 export const authRouter = router;
